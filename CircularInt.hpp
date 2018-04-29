e436bde87f4ee2a002a0577dc17bcba553266d3a
Orimol <33389705+Orimol@users.noreply.github.com>
2018-04-29 04:50:58 -0700

    Add files via upload

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -1,55 +1,76 @@
+//
+// Created by ori on 4/22/18.
+//
+
+#ifndef EX4_CIRCULARINT_HPP
+#define EX4_CIRCULARINT_HPP
+
+#endif //EX4_CIRCULARINT_HPP
 #include <iostream>
 using namespace std;
 
-class CircularInt{
+class CircularInt {
 private:
-  int value;
-  int lowLimit;
-  int highLimit;
-
+    int lowlimit, highlimit;
+    int value;
 public:
-//Constractor and Destractor
-  CircularInt (int x, int y);
-  ~CircularInt();
+    CircularInt(int x, int y);
+    CircularInt& operator = (int num);
 
-//Methods
+    CircularInt& operator += (int num);
 
-friend std::ostream& operator<<(std::ostream& o, CircularInt const& ci);
+    CircularInt& operator += (CircularInt ci);
 
-CircularInt operator + (int num);
+    CircularInt operator + (int num);
 
-CircularInt operator + (CircularInt const ci);
+    CircularInt operator + (CircularInt ci);
 
-CircularInt& operator += (int num);
+    CircularInt& operator -= (int num);
 
-CircularInt& operator += (CircularInt const& ci);
+    CircularInt& operator -= (CircularInt ci);
 
-CircularInt operator - (int num);
+    CircularInt operator - (int num);
 
-CircularInt operator - (CircularInt const ci);
+    CircularInt operator - (CircularInt& ci);
 
-CircularInt& operator -= (int num);
+    friend CircularInt& operator-(int num, CircularInt& ci);
 
-CircularInt& operator -= (CircularInt const ci);
+    CircularInt operator - ();
 
-CircularInt& operator *= (int num);
+    CircularInt& operator *= (int num);
 
-CircularInt& operator *= (CircularInt const ci);
+    CircularInt& operator *= (CircularInt ci);
 
-CircularInt& operator ++ (int);
+    CircularInt operator * (int num);
 
-CircularInt& operator --(int);
+    CircularInt operator * (CircularInt ci);
 
-CircularInt operator - ();
+    CircularInt& operator ++ ();
 
-CircularInt operator / (int num);
+    CircularInt& operator ++ (int num);
 
-CircularInt operator ^(int num);
+    CircularInt& operator -- ();
 
-CircularInt operator ^(CircularInt const ci);
+    CircularInt& operator / (int num);
 
-bool operator ==(int num);
+    friend ostream& operator << (ostream& os, CircularInt ci);
 
-friend CircularInt operator - (int num, const CircularInt& ci);
+    /*  CircularInt operator ++ (CircularInt ci) {
+          CircularInt result(ci.lowlimit, ci.highlimit);
+          result.value =ci.value+1;
+          if(result.value>ci.highlimit)
+              result.value=ci.lowlimit;
+          return result;
+      }
+
+      CircularInt operator -- (CircularInt ci) {
+          CircularInt result(ci.lowlimit, ci.highlimit);
+          result.value =ci.value-1;
+          if(result.value<ci.lowlimit)
+              result.value=ci.highlimit;
+          return result;
+      }
+  */
+
 
 };
